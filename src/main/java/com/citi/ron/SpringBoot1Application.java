f7a4b18cb0bb0048db312404bfed3bb77c0f679b
Ron R. Pan <rpan93@gmail.com>
2017-01-14 10:49:54 -0500

    first commit

---
+++ src/main/java/com/citi/ron/SpringBoot1Application.java
@@ -0,0 +1,12 @@
+package com.citi.ron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1Application.class, args);
+	}
+}
